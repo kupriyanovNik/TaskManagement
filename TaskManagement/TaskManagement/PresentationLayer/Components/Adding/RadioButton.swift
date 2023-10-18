73f92aaa2d46988d3263a66cdbc389ccb2b00177
kupriyanovNik <cucuprianov@icloud.com>
2023-10-18 20:52:04 +0300

    prepared for sending notifications

---
+++ TaskManagement/TaskManagement/PresentationLayer/Components/Adding/RadioButton.swift
@@ -0,0 +1,41 @@
+//
+//  RadioButton.swift
+//
+
+import SwiftUI
+
+struct RadioButton: View {
+
+    // MARK: - Property Wrappers
+    @Binding var isSelected: Bool
+
+    // MARK: - Internal Properties
+    var accentColor: Color
+
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(accentColor, style: .init(lineWidth: 2))
+            if isSelected {
+                Circle()
+                    .fill(accentColor)
+                    .padding(2)
+                    .overlay {
+                        Image(systemName: "checkmark")
+                            .foregroundColor(.white)
+                    }
+                    .transition(.opacity.combined(with: .scale))
+            }
+        }
+        .onTapGesture {
+            isSelected.toggle()
+        }
+        .animation(.default, value: isSelected)
+    }
+}
+
+#Preview {
+    RadioButton(isSelected: .constant(true), accentColor: .purple)
+        .frame(width: 30, height: 30)
+}
