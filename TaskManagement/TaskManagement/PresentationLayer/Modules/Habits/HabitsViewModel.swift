4c0af97701dabcc333eed54de9b4cbd303c59098
kupriyanovNik <cucuprianov@icloud.com>
2023-11-04 20:10:27 +0300

    removed Unused code

--- TaskManagement/TaskManagement/PresentationLayer/Modules/Habits/HabitsViewModel.swift
+++ TaskManagement/TaskManagement/PresentationLayer/Modules/Habits/HabitsViewModel.swift
@@ -5,39 +5,7 @@
 import Foundation
 
 class HabitsViewModel: ObservableObject {
-    @Published var showGreetings: Bool = true 
+    @Published var showGreetings: Bool = true
 
-    @Published var weekDays: [String] = []
-
-    @Published var currentWeek: [Date] = []
-
-    private var calendar = Calendar.current
-
-    init() {
-        fetchCurrentWeek()
-    }
-
-    func fetchCurrentWeek() {
-        weekDays = []
-        currentWeek = []
-
-        let today = Date()
-        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
-        guard let firstWeekDay = week?.start else { return }
-
-        (0...6).forEach {
-            if let weekday = calendar.date(byAdding: .day, value: $0, to: firstWeekDay) {
-                let extractedWeekday = extractDate(date: weekday, format: Constants.DateFormats.forDateLiteral)
-
-                self.currentWeek.append(weekday)
-                self.weekDays.append(extractedWeekday)
-            }
-        }
-    }
     
-    func extractDate(date: Date, format: String) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = format
-        return formatter.string(from: date)
-    }
 }
