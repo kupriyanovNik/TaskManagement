c6516085bc6f642a877bb7e822f0616612e44fc4
kupriyanovNik <cucuprianov@icloud.com>
2023-11-23 15:19:56 +0300

    added HabitAddingViewModel unit tests

--- TaskManagement/TaskManagementTests/HabitAddingViewModelTests.swift
+++ TaskManagement/TaskManagementTests/HabitAddingViewModelTests.swift
@@ -1,8 +1,32 @@
 //
 //  HabitAddingViewModelTests.swift
-//  TaskManagementTests
-//
-//  Created by Никита Куприянов on 23.11.2023.
 //
 
-import Foundation
+
+import XCTest
+@testable import TaskManagement
+
+final class HabitAddingViewModelTests: XCTestCase {
+
+    // MARK: - Private Properties
+
+    private var viewModel: HabitAddingViewModel!
+
+    // MARK: - Internal Functions
+
+    override func setUp() {
+        super.setUp()
+        viewModel = HabitAddingViewModel()
+    }
+
+    func testIsAbleToSave() {
+        viewModel.shouldNotificate = true 
+        XCTAssert(!viewModel.isAbleToSave())
+        viewModel.reminderText = "    a"
+        XCTAssert(!viewModel.isAbleToSave())
+        viewModel.habitTitle = " a"
+        XCTAssert(!viewModel.isAbleToSave())
+        viewModel.weekDaysIndicies = [0, 1, 4]
+        XCTAssert(viewModel.isAbleToSave())
+    }
+}
